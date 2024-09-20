01e4ccbac7b513794896cbdd96465d28af7e20c8
Flavia1897 <flaviaamunsiimiire@gmail.com>
2024-09-20 13:41:20 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+
+<?php
+
+$name="AMUNSIIMIIRE FLAVIA";
+$DOB="22/02/2001";
+$Address="RUKIGA";
+$FYear=2024;
+$BYear=2001;
+$Age=$FYear-$BYear;
+echo"Name: ".$name. "<br>";
+echo"Date Of Birth : ".$DOB."<br>";
+echo"Home Address: ".$Address."<br>";
+echo"Welcome home, ".$name."<br>";
+echo"You are now , ".$Age."Years old";
+/* DOB stands for date of birth,
+FYEAR stands for current year
+BYEAR stands for year of birth*/
+?> 
+
